b7782636a8669a8212a2827579dfb5e25ce79f06
marcos <marcosrodriguesbrugnaro@gmail.com>
2021-07-28 17:16:04 -0300

    Begin enable cors

---
+++ readme.md
@@ -0,0 +1,3 @@
+yarn typeorm migration:create -n migration
+yarn typeorm migration:run
+yarn typeorm migration:revert
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,11 +1,11 @@
-import { getRepository } from 'typeorm'
-import { compare } from 'bcryptjs'
-import { sign } from 'jsonwebtoken'
-import authConfig from '../config/auth'
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
-import AppError from '../errors/AppError'
+import AppError from '../errors/AppError';
 
-import User from '../models/User'
+import User from '../models/User';
 
 interface Request {
   email: string;
@@ -19,33 +19,32 @@ interface Response {
 
 class AuthenticateUserService {
   public async execute({ email, password }: Request): Promise<Response> {
-    const usersRepository = getRepository(User)
+    const usersRepository = getRepository(User);
 
-    const user = await usersRepository.findOne({ where: { email } })
+    const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new AppError('Incorrect email/password combination.', 401)
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    const passwordMatched = await compare(password, user.password)
+    const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new AppError('Incorrect email/password combination.', 401)
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    const { secret, expiresIn } = authConfig.jwt
+    const { secret, expiresIn } = authConfig.jwt;
 
     const token = sign({}, secret, {
       subject: user.id,
-      expiresIn
-    })
+      expiresIn: '1d',
+    });
 
     return {
       user,
-      token
-    }
-
+      token,
+    };
   }
 }
 
-export default AuthenticateUserService
+export default AuthenticateUserService;
